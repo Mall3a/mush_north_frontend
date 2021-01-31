075c6c46a5dd2994d2cc3e96791dca42c15fc368
Constanza Mallea <comallea@gmail.com>
2021-01-31 12:11:56 -0300

    Se agrega boton a home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,24 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./index.module.css";
 import backgroundImg from "../../images/background.jpg";
-
+import { Link } from "react-router-dom";
 const Home = (props) => {
   return (
     <div>
       <section className={styles.backgroundImg}>
-        <div>
-          <div className={styles.infoBox}>
-            <h3>Champiñones Ostras</h3>
-            <p>
-              Nos especializamos en la producción de hongos comestibles con alto
-              contenido nutricional.
-            </p>
-          </div>
+        <div className={styles.infoBox}>
+          <h3>Champiñones Ostras</h3>
+          <p>
+            Nos especializamos en la producción de hongos comestibles con alto
+            contenido nutricional.
+          </p>
+
+          <Link to="/shop">
+            <button className={styles.button}>Comprar</button>
+          </Link>
         </div>
       </section>
-      {/* <section className={styles.backgroundImg}>
-        <div className={styles.container}>asdasd</div>
-      </section> */}
     </div>
   );
 };
